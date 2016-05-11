5a181fa8c23d9cf289fed92b981b6c23d744ced3
hsmouc <heshiming001@icloud.com>
2016-05-11 21:08:00 +0800

    fix the bugs

--- Calculator/USR/main.c
+++ Calculator/USR/main.c
@@ -17,6 +17,7 @@ int temp_useless;
 int temp;
 extern int num_temp;
 void main(){
+	unsigned char tempKeycode;
 	hc165_init();
 	I2C_Init();
 	LcdInit();
@@ -26,14 +27,16 @@ void main(){
 		if(key_press == 1 && step == 0) {
 			key_press = 0;
 			tempKeytransfer_1 = keyscan_transfer(key_code);
-			if(key_code != 0x04) {
+			if(key_code != 0x04 && key_code != 0x08 && key_code != 0x0C) {
 				tempNumtransfer_1 = number_transfer(tempKeytransfer_1);
 				disp_line(0);
 				temp = lcd_numdisplay(tempNumtransfer_1);
+				LcdWrCmd(0x80+temp);
 			}
 		}
 		if(key_press == 1 && step == 1) {
 			key_press = 0;
+			tempKeycode = key_code;
 			lcd_flagdisplay(keyscan_transfer(key_code));
 			num_temp = 0;
 		}
@@ -47,7 +50,7 @@ void main(){
 			}
 		}
 		if(step == 3){	
-			result = number_calculate(tempNumtransfer_1,tempNumtransfer_2,0x08);
+			result = number_calculate(tempNumtransfer_1,tempNumtransfer_2,tempKeycode);
 			disp_line(1);
 			temp_useless = lcd_numdisplay(result);	
 		}
